d91b51f409905c251fa6a54e38911faa2cd61de0
ddgithub@gmail.com <ddgithub@gmail.com>
2015-07-12 10:34:33 -0700

    change the strcture and controller

--- app/controllers/TicketsController.php
+++ app/controllers/TicketsController.php
@@ -31,27 +31,31 @@ class TicketsController extends BaseController {
 		foreach ($documents_group as $key => $value) {
 			if($value != "." && $value != ".."){
 				$handler = new handler($dir,$value);
-				
-				try {
-		            $document = Document::create(array(
-		                'path' 			=> $handler->getPath(),
-		                'fileName'  	=> $handler->getFileName(),
-		                'fileType'  	=> $handler->getFileType(),
-		                'systemName'    => $handler->getSystemName(),
-		                'airlineName'   => $handler->getAirlineName(),
-		                'tickeNumebr'   => $handler->getTickeNumebr(),
-		                'dateString'    => $handler->getDateString(),
-		                'orderOfDay'    => $handler->getOrderOfDay(),
-		                'fileContent'   => $handler->getFileContent(),
-		                'dateOfFile'    => $handler->getDateOfFile(),
-		            ));
-		            $document->save();
-		            $num++;
-		            rename($dir.$value, "../done/".$value);
-		        } catch (Exception $e) {
-		            $response['info'] = "fail";
-		            $boolean = false;
-		            echo $e;
+				if($handler->getFileType() != NULL){
+					try {
+			            $document = Document::create(array(
+			                'path' 			=> $handler->getPath(),
+			                'fileName'  	=> $handler->getFileName(),
+			                'fileType'  	=> $handler->getFileType(),
+			                'systemName'    => $handler->getSystemName(),
+			                'airlineName'   => $handler->getAirlineName(),
+			                'tickeNumebr'   => $handler->getTicketNumber(),
+			                'dateString'    => $handler->getDateString(),
+			                'orderOfDay'    => $handler->getOrderOfDay(),
+			                'fileContent'   => $handler->getFileContent(),
+			                'dateOfFile'    => $handler->getDateOfFile(),
+			                'paxName'		=> $handler->getPaxName(),
+			                'rloc'			=> $handler->getRloc(),
+			                'ticketsType'	=> $handler->getTicketsType(),
+			            ));
+			            $document->save();
+			            $num++;
+			            rename($dir.$value, "../done/".$value);
+			        } catch (Exception $e) {
+			            $response['info'] = "fail";
+			            $boolean = false;
+			            echo $e;
+			        }
 		        }				
 			}
 		}
